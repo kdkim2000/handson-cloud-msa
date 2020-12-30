a0d2692955b55cc35e0ff89a71e1c7eaf6d32e05
ueong <dnfod@me.com>
2020-12-30 17:13:46 +0900

    nats -> rabbitmq

--- eshop-backend/src/main/java/com/samsungsds/eshop/order/OrderController.java
+++ eshop-backend/src/main/java/com/samsungsds/eshop/order/OrderController.java
@@ -16,6 +16,7 @@ import com.samsungsds.eshop.shipping.ShippingService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,17 +32,20 @@ public class OrderController {
     private final CartService cartService;
     private final PaymentService paymentService;
     private final ProductService productService;
+    private final RabbitTemplate rabbitTemplate;
 
     public OrderController(final OrderService orderService, 
     final ShippingService shippingService,
     final  PaymentService paymentService,
     final CartService cartService,
-    final ProductService productService) {
+    final ProductService productService,
+    final RabbitTemplate rabbitTemplate) {
         this.orderService = orderService;
         this.shippingService = shippingService;
         this.paymentService = paymentService;
         this.cartService = cartService;
         this.productService = productService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @PostMapping(value = "/orders")
@@ -78,7 +82,8 @@ public class OrderController {
         String orderId = orderService.createOrderId(orderRequest);
 
         // 카트 비우기
-        cartService.emptyCart();
+        // cartService.emptyCart();
+        rabbitTemplate.convertAndSend("eshop-exchange","order.placed", new OrderPlaced(orderId));
         return ResponseEntity.ok(new OrderResult(orderId, shippingResult.getShippingTrackingId(),
                 shippingResult.getShippingCost(), totalCost));
     }
