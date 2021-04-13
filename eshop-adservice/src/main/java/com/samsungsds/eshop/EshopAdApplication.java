0ffc48df1fd0022f5a98ce3b97be00874e70772c
milo <kajsss@gmail.com>
2021-04-13 09:39:11 +0900

    Recover AD Service

--- eshop-adservice/src/main/java/com/samsungsds/eshop/EshopAdApplication.java
+++ eshop-adservice/src/main/java/com/samsungsds/eshop/EshopAdApplication.java
@@ -1,13 +1,8 @@
 package com.samsungsds.eshop;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
-
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,11 +10,5 @@ public class EshopAdApplication {
     public static void main(String[] args) {
         SpringApplication.run(EshopAdApplication.class, args);
     }
-
-    @RabbitListener(queues = "ad.queue")
-    public void receiveMessage(final String message) {
-        System.out.println(message);
-    }
-
 
 }
